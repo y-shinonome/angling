f8bdecb4f6bd8b71b31adb002cafee1fab2f35c3
y-shinonome <y.shinonomail@gmail.com>
2022-04-06 13:36:31 +0900

    update: ディレクトリ構成変更

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Index: NextPage = () => {
+  return (
+    <div>
+      <p className="text-xl font-bold text-red-500">hello,World!</p>
+    </div>
+  )
+}
+
+export default Index
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ['./pages/**/*.tsx'],
+  content: ['./src/pages/**/*.tsx'],
   theme: {
     extend: {},
   },
