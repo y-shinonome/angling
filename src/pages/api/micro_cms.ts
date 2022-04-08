1edc4c788fea03d2f4c32b11187dc8213186af54
y-shinonome <y.shinonomail@gmail.com>
2022-04-08 19:32:38 +0900

    update: リファクタリング

--- src/pages/api/micro_cms.ts
+++ src/pages/api/micro_cms.ts
@@ -1,7 +1,7 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { /*NextApiRequest,*/ NextApiResponse } from 'next'
 import { client } from '../../utils/micro_cms'
 
-const updateTopPage = async (req: NextApiRequest, res: NextApiResponse) => {
+const MicroCmsApi = async (/*req: NextApiRequest,*/ res: NextApiResponse) => {
   const data = await client.get({
     endpoint: 'angling_spots',
     queries: { limit: 1000 },
@@ -9,4 +9,4 @@ const updateTopPage = async (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json(data.contents)
 }
 
-export default updateTopPage
+export default MicroCmsApi
