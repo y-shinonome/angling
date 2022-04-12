b613a10bc10f6b78fafba0e00259d5788f68e021
y-shinonome <y.shinonomail@gmail.com>
2022-04-12 20:46:32 +0900

    update: 不要ファイル、ライブラリ整理

--- src/pages/api/firestore.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { getAnglingSpot } from '../../utils/firestore'
-
-const FirestoreApi = async (_req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json(await getAnglingSpot())
-}
-
-export default FirestoreApi
