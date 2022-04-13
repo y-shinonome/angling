fa42229fbe86660868344b08ecc96e7d7f50d63d
y-shinonome <y.shinonomail@gmail.com>
2022-04-13 09:57:50 +0900

    update: attributionの表示位置変更

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,4 +1,9 @@
-import { MapContainer, TileLayer, ScaleControl } from 'react-leaflet'
+import {
+  MapContainer,
+  TileLayer,
+  ScaleControl,
+  AttributionControl,
+} from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 
 type Props = {
@@ -8,20 +13,24 @@ type Props = {
 
 const AnglingSpot: React.FC<Props> = ({ center, zoom, children }) => {
   return (
-    <MapContainer
-      center={center}
-      zoom={zoom}
-      style={{ height: '50vh', width: '100%' }}
-    >
-      <TileLayer
-        maxNativeZoom={19}
-        maxZoom={20}
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <ScaleControl />
-      {children}
-    </MapContainer>
+    <div className="h-[50vh]">
+      <MapContainer
+        center={center}
+        zoom={zoom}
+        style={{ height: '100%', width: '100%' }}
+        attributionControl={false}
+      >
+        <TileLayer
+          maxNativeZoom={19}
+          maxZoom={20}
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <AttributionControl position="topright" />
+        <ScaleControl />
+        {children}
+      </MapContainer>
+    </div>
   )
 }
 
