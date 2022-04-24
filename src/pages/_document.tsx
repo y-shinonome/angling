ebd2c31316aef10f794ffdfb1366ef5f875cdfcb
y-shinonome <y.shinonomail@gmail.com>
2022-04-24 15:57:14 +0900

    update: カスタムDocument追加

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+function Document() {
+  return (
+    <Html>
+      <Head lang="ja-JP" />
+      <body className="bg-[#f9f9f9] text-[#2a2a2a]">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
+export default Document
