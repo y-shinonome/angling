0990d003818df471e4aba2c225309ee34a11a7d4
y-shinonome <y.shinonomail@gmail.com>
2022-04-24 16:57:31 +0900

    update: フォントの変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./src/components/**/*.{html,tsx}', './src/pages/**/*.{html,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': [
+          'Helvetica Neue',
+          'Arial',
+          'Hiragino Kaku Gothic ProN',
+          'Hiragino Sans',
+          'Meiryo',
+          'sans-serif',
+          ...defaultTheme.fontFamily.sans,
+        ],
+      },
+    },
   },
   plugins: [],
 }
