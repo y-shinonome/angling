3d8c84c4cf4b6137d044b171e02640bdf3bd7ca0
y-shinonome <y.shinonomail@gmail.com>
2022-04-28 21:11:46 +0900

    update: topページデザイン修正

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 function Document() {
   return (
     <Html>
-      <Head lang="ja-JP" />
+      <Head lang="ja-JP">
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poiret+One&display=optional"
+          rel="stylesheet"
+        />
+      </Head>
       <body className="bg-[#f9f9f9] text-[#2a2a2a]">
         <Main />
         <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
+import Image from 'next/image'
 
 const Index: NextPage = () => {
   return (
     <>
-      <p className="mb-1 pt-20 text-center text-xl">釣り場探索マップ</p>
-      <h1 className="mb-20 text-center text-5xl">Langling</h1>
+      <p className="mb-1 mt-14 text-center text-lg">釣り場探索マップ</p>
+      <h1 className="mb-10 text-center font-['Poiret_One'] text-6xl">
+        Langling
+      </h1>
+      <div className="relative mx-auto h-[200px] w-[200px]">
+        <Image src="/top.png" alt="top" layout="fill" objectFit="contain" />
+      </div>
       <Link href={`/angling_map`}>
-        <a className="mx-auto block w-[fit-content] rounded-lg bg-[#ADFFEA] px-5 py-2">
+        <a className="mx-auto mt-10 block w-[fit-content] rounded-lg bg-[#ADFFEA] px-5 py-2">
           地図を開く
         </a>
       </Link>
