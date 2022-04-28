f98f93272c151e721955777fc23c1a913d227c17
y-shinonome <y.shinonomail@gmail.com>
2022-04-29 04:19:12 +0900

    update: top画像のプレースホルダーとしてblurを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,17 +5,24 @@ import Image from 'next/image'
 const Index: NextPage = () => {
   return (
     <>
-      <p className="mb-1 mt-14 text-center text-lg">釣り場探索マップ</p>
+      <p className="mt-14 text-center text-lg">釣り場探索マップ</p>
       <h1 className="mb-10 text-center font-logo text-6xl">Langling</h1>
-      <div className="relative mx-auto h-[200px] w-[200px]">
-        <Image src="/top.png" alt="top" layout="fill" objectFit="contain" />
+      <div className="relative mx-auto mb-5 h-[200px] w-[200px]">
+        <Image
+          src="/top.png"
+          alt="top"
+          layout="fill"
+          objectFit="contain"
+          placeholder="blur"
+          blurDataURL="/top.png"
+        />
       </div>
       <Link href={`/angling_map`}>
-        <a className="mx-auto mt-10 block w-[fit-content] rounded-lg bg-[#ADFFEA] px-5 py-2">
+        <a className="mx-auto mb-20 block w-[fit-content] rounded-lg bg-[#ADFFEA] px-5 py-2">
           地図を開く
         </a>
       </Link>
-      <h2 className="mt-10 mb-5 text-center text-2xl">このWebサイトについて</h2>
+      <h2 className="mb-5 text-center text-2xl">このWebサイトについて</h2>
       <p className="text-center">
         サンプルテキストサンプルテキスト
         <br />
