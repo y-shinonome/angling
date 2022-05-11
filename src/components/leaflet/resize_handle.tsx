68a17ec0421ddbf518eef4081c7623753a139515
y-shinonome <y.shinonomail@gmail.com>
2022-05-11 20:13:09 +0900

    update: リサイズハンドルのアニメーション削除

--- src/components/leaflet/resize_handle.tsx
+++ src/components/leaflet/resize_handle.tsx
@@ -1,7 +1,7 @@
 const ResizeHandle: React.FC = () => {
   return (
     <div className="h-6">
-      <div className="relative top-[65%] mx-auto h-1 w-16 animate-pulse rounded-sm bg-gray-500/50 shadow-md shadow-gray-500/50" />
+      <div className="relative top-[65%] mx-auto h-1 w-16 rounded-sm bg-gray-400/50" />
     </div>
   )
 }
