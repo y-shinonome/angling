f8680f7c1aa95cc72ec1b15238522097b91f419b
y-shinonome <y.shinonomail@gmail.com>
2022-05-11 20:31:15 +0900

    update: サイドバーの開閉処理変更

--- src/components/template/navigation.tsx
+++ src/components/template/navigation.tsx
@@ -1,40 +1,37 @@
-import { useState } from 'react'
+import { useToggle } from 'react-use'
 import Link from 'next/link'
 import SVG from '../../components/atoms/svg'
 
 const Navigation: React.FC = () => {
-  const [isOpen, setIsOpen] = useState(false)
-  const toggleSidebar = () => {
-    setIsOpen(!isOpen)
-  }
+  const [open, toggle] = useToggle(false)
 
   return (
     <>
       <button
         aria-label="menu"
         className="fixed top-2 right-2 z-30 rounded-md bg-[#f9f9f9]/50 p-2 shadow backdrop-blur-[2px] duration-300 hover:bg-black/5"
-        onClick={toggleSidebar}
+        onClick={toggle}
       >
         <SVG
-          src={`${isOpen ? '/icons/close.svg' : '/icons/menu.svg'}`}
+          src={`${open ? '/icons/close.svg' : '/icons/menu.svg'}`}
           className="h-7 w-7"
         />
       </button>
       <div
-        onClick={toggleSidebar}
+        onClick={toggle}
         className={`fixed left-0 top-0 z-20 h-[100vh] w-[100vw] duration-300 ${
-          isOpen ? 'bg-white/[1%] backdrop-blur-[2px]' : 'pointer-events-none'
+          open ? 'bg-white/[1%] backdrop-blur-[2px]' : 'pointer-events-none'
         }`}
       />
       <nav
-        className={`fixed right-0 top-0 z-20 h-[100vh] max-w-[400px] overflow-hidden whitespace-nowrap bg-[#f9f9f9] pt-20 shadow-[0_0_15px_0_rgba(0,0,0,0.2)] duration-300 ${
-          isOpen ? 'w-[75%]' : 'w-0'
+        className={`fixed right-0 top-0 z-20 h-[100vh] max-w-[400px] overflow-hidden whitespace-nowrap bg-[#f9f9f9] pt-20 shadow-[0_0_15px_0_rgba(0,0,0,0.3)] duration-500 ${
+          open ? 'w-[75%]' : 'w-0'
         } `}
       >
         <Link href="/">
           <a
             className="ml-2 mb-5 block border-b border-black/10 pl-2"
-            onClick={toggleSidebar}
+            onClick={toggle}
           >
             <div className="flex items-center">
               <SVG
@@ -48,7 +45,7 @@ const Navigation: React.FC = () => {
         <Link href="/angling_map">
           <a
             className="ml-2 mb-5 block border-b border-black/10 pl-2"
-            onClick={toggleSidebar}
+            onClick={toggle}
           >
             <div className="flex items-center">
               <SVG
@@ -62,7 +59,7 @@ const Navigation: React.FC = () => {
         <Link href="/">
           <a
             className="ml-2 mb-5 block border-b border-black/10 pl-2"
-            onClick={toggleSidebar}
+            onClick={toggle}
           >
             <div className="flex items-center">
               <SVG
@@ -76,7 +73,7 @@ const Navigation: React.FC = () => {
         <Link href="/">
           <a
             className="ml-2 mb-5 block border-b border-black/10 pl-2"
-            onClick={toggleSidebar}
+            onClick={toggle}
           >
             <div className="flex items-center">
               <SVG
@@ -90,7 +87,7 @@ const Navigation: React.FC = () => {
         <Link href="/">
           <a
             className="ml-2 mb-5 block border-b border-black/10 pl-2"
-            onClick={toggleSidebar}
+            onClick={toggle}
           >
             <div className="flex items-center">
               <SVG
@@ -104,7 +101,7 @@ const Navigation: React.FC = () => {
         <Link href="/">
           <a
             className="ml-2 mb-5 block border-b border-black/10 pl-2"
-            onClick={toggleSidebar}
+            onClick={toggle}
           >
             <div className="flex items-center">
               <SVG
