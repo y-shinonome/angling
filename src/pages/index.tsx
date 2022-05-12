8bb1ba06646b8d7f26d85108841362be108d58f2
y-shinonome <y.shinonomail@gmail.com>
2022-05-12 15:06:19 +0900

    update: トップページデザイン修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,10 @@ import type { ReactElement } from 'react'
 import type { NextPage } from 'next'
 import { GetStaticProps } from 'next'
 import Link from 'next/link'
-import Image from 'next/image'
 import type { Entry } from 'contentful'
 import type { IAnglingFieldsFields } from '../../@types/contentful'
 import { getAnglingFields } from '../utils/contentful'
+import Layout from '../components/template/layout'
 import SVG from '../components/atoms/svg'
 import Leaflet from '../components/template/leaflet'
 
@@ -20,78 +20,60 @@ type Props = {
 const TopPage: NextPageWithLayout = () => {
   return (
     <>
-      <div className="mx-auto max-w-[600px] px-3">
-        <p className="mt-10">釣り場探索マップ</p>
-        <h1 className="font-logo relative right-[3px] mb-10 text-5xl font-light text-[#83beaf]">
+      <div className="mb-10 flex flex-col items-center py-20">
+        <p className="text-sm">釣り場探索マップ</p>
+        <h1 className="font-logo mb-10 text-4xl font-light text-[#83beaf]">
           Langling
         </h1>
-        <div className="relative mb-5 aspect-[1.919/1] w-full opacity-70">
-          <Image
-            src="/images/top.png"
-            alt="top"
-            layout="fill"
-            objectFit="cover"
-            placeholder="blur"
-            blurDataURL="/images/top.png"
-          />
-        </div>
         <Link href={`/angling_map`}>
-          <a className="mx-auto mb-16 flex w-[100%] items-center justify-center bg-[#c5fff0] py-2 text-lg shadow-md shadow-gray-400/50 duration-300 hover:bg-black/10">
-            <SVG src="/icons/map.svg" className="mr-2 h-[24px] w-[24px]" />
-            地図を開く
+          <a className="flex w-[fit-content] items-center justify-center rounded bg-teal-100 px-8 py-2 text-lg shadow-md shadow-gray-400/40 duration-300 hover:bg-black/10">
+            <SVG src="/icons/map.svg" className="mr-2 h-[22px] w-[22px]" />
+            釣り場を探す
           </a>
         </Link>
-        <h2 className="mb-2 border-b-[4px] border-[#c5fff0] text-xl text-[#333333]">
-          このサイトについて
-        </h2>
-        <p className="text-sm">
-          釣り場の情報を地図にまとめたWebサイトです。
-          <br />
-          釣りポイントの情報だけではなく
-          <br />
-          駐車場やトイレ等の場所も探すことができます。
-          <br />
-          <br />
-          地図上のピンアイコンを押すと
-          <br />
-          その場所の情報を確認することができます。
-        </p>
-        <SVG
-          src="/icons/pins.svg"
-          className="my-5 h-[160px] w-[182px] opacity-80"
-        />
-        <p className="text-sm">
-          まだ行ったことのない釣り場を
-          <br />
-          地図から探索してみませんか？
-        </p>
-        <h2 className="mt-16 mb-2 border-b-4 border-[#c5fff0] text-xl text-[#333333]">
-          更新情報
-        </h2>
-        <ul className="text-sm">
-          <li className="mb-2 flex">
-            <time dateTime="2022-04-30" className="mr-4 flex-shrink-0">
-              2022年4月30日
-            </time>
-            <p>末広水際線プロムナード（ふれーゆ裏） 釣り場マップ公開</p>
-          </li>
-          <li className="mb-2 flex">
-            <time
-              dateTime="2022-04-30"
-              className="mr-4 flex-shrink-0 flex-grow-0"
-            >
-              2022年4月30日
-            </time>
-            <p className="flex-grow-0">東扇島西公園 釣り場マップ公開</p>
-          </li>
-          <li className="mb-2 flex">
-            <time dateTime="2022-04-30" className="mr-4 flex-shrink-0">
-              2022年4月29日
-            </time>
-            <p>Webサイト公開</p>
-          </li>
-        </ul>
       </div>
+
+      <h2 className="mb-2 border-b-[4px] border-[#c5fff0] text-xl text-[#333333]">
+        このサイトについて
+      </h2>
+      <p className="text-sm">
+        釣り場の情報を地図にまとめたWebサイトです。
+        <br />
+        釣りポイントの情報だけではなく
+        <br />
+        駐車場やトイレ等の場所も探すことができます。
+        <br />
+        <br />
+        まだ行ったことのない釣り場を
+        <br />
+        地図から探索してみませんか？
+      </p>
+      <h2 className="mt-16 mb-2 border-b-4 border-[#c5fff0] text-xl text-[#333333]">
+        更新情報
+      </h2>
+      <ul className="text-sm">
+        <li className="mb-2 flex">
+          <time dateTime="2022-04-30" className="mr-4 flex-shrink-0">
+            2022年4月30日
+          </time>
+          <p>末広水際線プロムナード（ふれーゆ裏） 釣り場マップ公開</p>
+        </li>
+        <li className="mb-2 flex">
+          <time
+            dateTime="2022-04-30"
+            className="mr-4 flex-shrink-0 flex-grow-0"
+          >
+            2022年4月30日
+          </time>
+          <p className="flex-grow-0">東扇島西公園 釣り場マップ公開</p>
+        </li>
+        <li className="mb-2 flex">
+          <time dateTime="2022-04-30" className="mr-4 flex-shrink-0">
+            2022年4月29日
+          </time>
+          <p>Webサイト公開</p>
+        </li>
+      </ul>
     </>
   )
 }
@@ -100,7 +82,9 @@ TopPage.getLayout = (props, page) => {
   return (
     <>
       <Leaflet anglingFields={props.anglingFields} zoom={10} />
-      <TopPage>{page}</TopPage>
+      <Layout>
+        <TopPage>{page}</TopPage>
+      </Layout>
     </>
   )
 }
