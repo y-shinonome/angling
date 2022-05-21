c3505995a63e06df9725cd1ee4af44fa6fd5502f
y-shinonome <y.shinonomail@gmail.com>
2022-05-21 16:52:58 +0900

    update: aboutページコンテンツ取得方法変更

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,21 +1,39 @@
 import type { NextPage } from 'next'
+import { GetStaticProps } from 'next'
+import ReactMarkdown from 'react-markdown'
+import { getDocments } from '../utils/contentful'
+import type { Entry } from 'contentful'
+import type { IDocumentsFields } from '../../@types/contentful'
 import Meta from '../components/molecules/meta'
 import Layout from '../components/template/layout'
 import TitleLogo from '../components/atoms/title_logo'
 
-const About: NextPage = () => {
+type Props = {
+  aboutContent: Entry<IDocumentsFields>
+}
+
+const About: NextPage<Props> = ({ aboutContent }) => {
   return (
     <>
       <Meta subTitle="このサイトについて" />
       <TitleLogo />
       <Layout>
         <section className="prose-custom mt-16">
-          <h1>このサイトについて</h1>
-          <p>準備中！</p>
+          <ReactMarkdown>{aboutContent.fields.content}</ReactMarkdown>
         </section>
       </Layout>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const aboutContent = await getDocments('about')
+
+  return {
+    props: {
+      aboutContent: aboutContent[0],
+    },
+  }
 }
 
 export default About
