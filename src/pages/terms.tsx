e8fcf2ca9cd60c521b596be21849454ebc86d381
y-shinonome <y.shinonomail@gmail.com>
2022-05-21 14:39:51 +0900

    update: 利用規約ページ修正

--- src/pages/terms.tsx
+++ src/pages/terms.tsx
@@ -1,21 +1,39 @@
 import type { NextPage } from 'next'
+import { GetStaticProps } from 'next'
+import ReactMarkdown from 'react-markdown'
+import { getDocments } from '../utils/contentful'
+import type { Entry } from 'contentful'
+import type { IDocumentsFields } from '../../@types/contentful'
 import Meta from '../components/molecules/meta'
 import Layout from '../components/template/layout'
 import TitleLogo from '../components/atoms/title_logo'
 
-const Terms: NextPage = () => {
+type Props = {
+  termsContent: Entry<IDocumentsFields>
+}
+
+const Terms: NextPage<Props> = ({ termsContent }) => {
   return (
     <>
       <Meta subTitle="利用規約" />
       <TitleLogo />
       <Layout>
         <section className="prose-custom mt-16">
-          <h1>利用規約</h1>
-          <p>準備中！</p>
+          <ReactMarkdown>{termsContent.fields.content}</ReactMarkdown>
         </section>
       </Layout>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const termsContent = await getDocments('terms')
+
+  return {
+    props: {
+      termsContent: termsContent[0],
+    },
+  }
 }
 
 export default Terms
