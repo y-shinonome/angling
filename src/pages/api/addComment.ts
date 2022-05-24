0521fc46d02708c2c9d29c4f967241e8beb4918c
y-shinonome <y.shinonomail@gmail.com>
2022-05-24 14:31:06 +0900

    update: コメント送信時にページの再検証処理実行

--- src/pages/api/addComment.ts
+++ src/pages/api/addComment.ts
@@ -4,6 +4,7 @@ import { setComment } from '../../utils/firestore'
 const addComent = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     await setComment(req.body)
+    await res.unstable_revalidate(`/angling_map/${req.body.pageId}`)
     res.status(200).json({})
   } catch (error: any) {
     res.status(500).json({})
