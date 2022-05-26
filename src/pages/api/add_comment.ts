8dc0ac73e8a02be1a441da6ee10f207f7c0b3054
y-shinonome <y.shinonomail@gmail.com>
2022-05-27 00:25:04 +0900

    update: コメント投稿時のクライアント接続情報取得処理修正

--- src/pages/api/add_comment.ts
+++ src/pages/api/add_comment.ts
@@ -5,15 +5,8 @@ import dayjs from 'dayjs'
 const addComment = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const comment = req.body
-
     comment.timestamp = dayjs().toDate()
     comment.headers = req.headers
-    comment.headers.realIp = req.headers['x-real-ip'] || ''
-    comment.headers.forwardedFor = req.headers['x-forwarded-for'] || ''
-    comment.headers.vercelIpCountry = req.headers['x-vercel-ip-country'] || ''
-    comment.headers.vercelIpCountryRegion =
-      req.headers['x-vercel-ip-country'] || ''
-    comment.headers.vercelIpCity = req.headers['x-vercel-ip-city'] || ''
 
     await setComment(comment)
     return res.status(200).end()
