453f078525cbb34d7d51ff5b1f0d1f17994fb167
y-shinonome <y.shinonomail@gmail.com>
2022-06-08 17:42:39 +0900

    update: twitter埋め込みコンポーネントサイズ修正

--- src/pages/react-social-media-embed.tsx
+++ src/pages/react-social-media-embed.tsx
@@ -22,10 +22,7 @@ const ReactSocialMediaEmbed: NextPage = () => {
       <Layout>
         <CustomFacebookEmbed url="https://www.facebook.com/fishingvision/photos/a.192837284176016/4939460569513640" />
         <div className="flex justify-center">
-          <TwitterEmbed
-            url="https://twitter.com/irohass_Fishing/status/1534266060592910336"
-            width="50%"
-          />
+          <TwitterEmbed url="https://twitter.com/irohass_Fishing/status/1534266060592910336" />
         </div>
         <div className="flex justify-center">
           <InstagramEmbed url="https://www.instagram.com/p/CegV2v0PpV4/" />
